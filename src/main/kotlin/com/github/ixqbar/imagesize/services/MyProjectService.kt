df45ce7e7c06313216daf7390047771a16e1d7c6
GitHub Action <action@github.com>
2025-03-11 01:18:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/ixqbar/imagesize/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ixqbar/imagesize/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.ixqbar.imagesize.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ixqbar.imagesize.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
